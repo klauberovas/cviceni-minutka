2bd368812902e71e7e155d69cdbe0c9526fab3a9
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-10-01 15:34:19 +0200

    Done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('funguju!');
+const alarmElm = document.querySelector('.alarm');
+const alarmAudio = document.querySelector('audio');
+
+const alarmRing = () => {
+  return alarmElm.classList.add('alarm--ring'), alarmAudio.play();
+};
+
+const timerId = setTimeout(
+  alarmRing,
+  prompt('Zadej za kolik sekund má začít budík zvonit.') + '000',
+);
+
+const buttonElm = document.querySelector('.button');
+buttonElm.addEventListener('click', () => {
+  clearTimeout(timerId), (alarmElm.innerHTML = '<h2>Budík zastaven.</h2>');
+});
